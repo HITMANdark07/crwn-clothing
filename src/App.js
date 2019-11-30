29534be96ede2d8edaf7eb7fa5ff9813b920ca51
HITMANdark07 <56204143+HITMANdark07@users.noreply.github.com>
2019-12-01 00:15:48 +0530

    custom button and formInput

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App(){
     return(
@@ -19,8 +14,8 @@ function App(){
         <Header/>
         <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
        </div>
     );
