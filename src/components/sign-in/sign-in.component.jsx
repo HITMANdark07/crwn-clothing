3fd6d4e6bbb8cd0f6c5130d284688cb4f7380e47
HITMANdark07 <56204143+HITMANdark07@users.noreply.github.com>
2019-12-01 17:44:05 +0530

    capitalize Email and password first letter

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -51,7 +51,7 @@ class SignIn extends React.Component{
                     type="email" 
                     value={this.state.email} 
                     handleChange={this.handleChange}
-                    label='email'
+                    label='Email'
                     required />
                     
                     <FormInput 
@@ -59,7 +59,7 @@ class SignIn extends React.Component{
                     type="password" 
                     value={this.state.password} 
                     handleChange={this.handleChange}
-                    label='password'
+                    label='Password'
                     required 
                     />
                     
