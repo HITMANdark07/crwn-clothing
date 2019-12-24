9b62abce0fefd0bd220a7267d198323c6c8c78cb
HITMANdark07 <56204143+HITMANdark07@users.noreply.github.com>
2019-12-24 13:54:06 +0530

    add stripe payment and add crownclothinf favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price*100;
+    const publishableKey = 'pk_test_QpdN6QyS1447Lfn9SNnxQq5T00k9ZUSv2H';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+
+    return(
+        <StripeCheckout 
+        label='Pay Now'
+        name='CRWN Clothing Ltd.'
+        billingAddress
+        shippingAddress
+        image='https://sendeyo.com/up/d/f3eb2117da'
+        description={`Your Total is $${price}`}
+        amount={priceForStripe}
+        panelLabel="Pay Now"
+        token={onToken}
+        stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
